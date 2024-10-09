308df10da4206c973a16118d74ede9f778344f5c
IlyaDOC <ilya22@inbox.ru>
2024-10-09 13:14:20 +0300

    added main section in index.html

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,33 @@
 $(document).ready(function () {
 
-  // INPUT MASK PHONE NUMBER
-  $('.phone-mask input').inputmask({
-    "mask": "+7 (999) 999-99-99",
-    showMaskOnHover: false,
-    showMaskOnFocus: false,
+    // INPUT MASK PHONE NUMBER
+    $('.phone-mask input').inputmask({
+        "mask": "+7 (999) 999-99-99",
+        showMaskOnHover: false,
+        showMaskOnFocus: false,
 
-    onBeforePaste: function (pastedValue, opts) {
-      return pastedValue.replace(/^8/, '');
-    }
-  });
+        onBeforePaste: function (pastedValue, opts) {
+            return pastedValue.replace(/^8/, '');
+        }
+    });
+    // moveElementsInFooter();
 });
 
-
+// function moveElementsInFooter() {
+//     const ulElement = document.querySelector('ul.footer__top-block');
+//     const liElements = ulElement.querySelectorAll('li');
+//     const elementsToMove = 6;
+//     let footerJsBlock;
+//
+//     liElements.forEach((element, index) => {
+//         if ((index + 1) % elementsToMove === 0) {
+//             footerJsBlock = document.createElement('div');
+//             footerJsBlock.appendChild(element);
+//             ulElement.appendChild(footerJsBlock);
+//         }
+//
+//         footerJsBlock.appendChild(element);
+//
+//     });
+//
+// }
