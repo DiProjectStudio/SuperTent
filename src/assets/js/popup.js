d54c2c47079af1dc012ad9b0f809d0d7440ee46d
IlyaDOC <ilya22@inbox.ru>
2024-10-17 10:20:58 +0300

    added popup

--- src/assets/js/popup.js
+++ src/assets/js/popup.js
@@ -3,7 +3,15 @@ $(document).ready(function () {
     // FANCYBOX OPTION
     Fancybox.bind('[data-fancybox]', {
         dragToClose: false,
-        autoFocus: false
+        autoFocus: false,
+        on: {
+            done: (fancybox, slide) => {
+                if (slide.src === '#popup-success-request') {
+                    setTimeout(() => {
+                        Fancybox.close();
+                    }, 3000);
+                }
+            },
+        },
     })
-
 });
